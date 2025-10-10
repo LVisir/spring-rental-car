954bcfaeba7bf686421055c0fa65eae98724e608
LVisir <dummymail97.lavoro@gmail.com>
2025-10-11 00:32:18 +0200

    update security config

--- src/main/java/it/si2001/rentalcar/security/SecurityConfig.java
+++ src/main/java/it/si2001/rentalcar/security/SecurityConfig.java
@@ -58,7 +58,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(
                 "/swagger-ui.html",
                 "/swagger-ui/**",
-                "/v3/api-docs/**"
+                "/v3/api-docs/**",
+                "/actuator/health"
         ).permitAll();
 
         http.authorizeRequests().antMatchers(GET, "/users", "/users/customers",
