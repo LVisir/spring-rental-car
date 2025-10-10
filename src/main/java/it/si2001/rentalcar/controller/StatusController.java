e8ed502ec9e45b4e342364e08834d863b972f70f
LVisir <dummymail97.lavoro@gmail.com>
2025-10-11 00:22:21 +0200

    add healthcheck

---
+++ src/main/java/it/si2001/rentalcar/controller/StatusController.java
@@ -0,0 +1,16 @@
+package it.si2001.rentalcar.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/actuator")
+public class StatusController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> status() {
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+
+}
