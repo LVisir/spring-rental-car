3c117aa838d0d89297d7ce6744d8ebd2cb219b67
LVisir <dummymail97.lavoro@gmail.com>
2025-10-11 00:28:36 +0200

    update status check

--- src/main/java/it/si2001/rentalcar/controller/StatusController.java
+++ src/main/java/it/si2001/rentalcar/controller/StatusController.java
@@ -3,9 +3,11 @@ package it.si2001.rentalcar.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/actuator")
+@RestController
+@RequestMapping("/actuator")
 public class StatusController {
 
     @GetMapping("/health")
